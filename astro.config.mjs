e636d09f8b2c361bdf4c3d4e4cf7b4b47ffc31c3
Sutee Suriyarueangwet <unlimited.tee@gmail.com>
2026-05-02 19:13:57 +0700

    chore: add MDX integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import tailwindcss from '@tailwindcss/vite';
 
 import svelte from '@astrojs/svelte';
 
+import mdx from '@astrojs/mdx';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
 
-  integrations: [svelte()]
+  integrations: [svelte(), mdx()]
 });
